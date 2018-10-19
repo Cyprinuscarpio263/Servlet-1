123bf0fbfaa83ccdfc1705e990fa1f2f824a85e8
Cyprinuscarpio263 <vaportrail263@hotmail.com>
2018-10-19 18:16:20 +0800

    modify AppListener.java

--- src/app08a/listener/AppListener.java
+++ src/app08a/listener/AppListener.java
@@ -20,6 +20,14 @@ public class AppListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent event) {
 		System.out.println("Listener init");
 		ServletContext sc = event.getServletContext();
+		String cPath = sc.getContextPath();
+		String rPath0 = sc.getRealPath("");
+		String rPath1 = sc.getRealPath("/");
+		StringBuilder bui = new StringBuilder();
+		bui.append("\tContextPath=").append(cPath).append("\n");
+		bui.append("\tRealPath()=").append(rPath0).append("\n");
+		bui.append("\tRealPath(/)=").append(rPath1);
+		System.out.println("app08a.listener.AppListener:\n" + bui.toString());
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("key1", "value1");
 		map.put("key2", "value2");
